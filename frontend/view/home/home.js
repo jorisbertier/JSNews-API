4a0d9effc390f8d8ddb77708e1e7b44b9b2f4f06
Joris Bertier <joris.bertier@gmail.com>
2024-04-24 14:49:01 -0500

    get favoris

--- frontend/view/home/home.js
+++ frontend/view/home/home.js
@@ -16,16 +16,16 @@ fetch(apiUrl)
         return response.json();
     })
     .then(userData => {
-        console.log('User Data:', userData);
+        // console.log('User Data:', userData);
         for(i = 0; i < userData.length; i++) {
             let div = document.createElement('div')
             let article = `<div class="col-12 mt-5">
-                                <div class="card article">
+                                <div class="card article article${userData[i].id}">
                                     <div class="card-header ">
                                         <h5 class="card-title d-flex justify-content-between">${userData[i].title}<span class="publication-date">${userData[i].publicationDate}</span></h5>
                                     </div>
                                     <img src="${userData[i].image}" class="card-img-top">
-                                    <span class="fa-stack fa-2x addFavorite">
+                                    <span class="fa-stack fa-2x addFavorite" data-article-id="${userData[i].id}" data-article-title="${userData[i].title}" data-article-content="${userData[i].content}" data-article-date="${userData[i].publicationDate}" data-article-img="${userData[i].image}">
                                         <i class="fas fa-star fa-stack-1x"></i>
                                         <i class="far fa-star fa-stack-1x"></i>
                                     </span>
@@ -36,8 +36,63 @@ fetch(apiUrl)
                             </div>`
         div.innerHTML = article;
         container.appendChild(div)
-
+        
         }
+
+        let favoris = document.querySelectorAll('.addFavorite')
+        let allFavorites = [];
+
+        favoris.forEach(function(favori) {
+            favori.addEventListener('click', ()=> {
+
+                let articleId = favori.dataset.articleId;
+                let articleTitle = favori.dataset.articleTitle;
+                let articleContent = favori.dataset.articleContent;
+                let articleDate = favori.dataset.articleDate;
+                let articleImg = favori.dataset.articleImg;
+
+                let existingIndex = allFavorites.findIndex(article => article.id === articleId);
+                
+                if (existingIndex !== -1) {
+                    allFavorites.splice(existingIndex, 1);
+                }else {
+                    allFavorites.push({
+                        id: articleId,
+                        title: articleTitle,
+                        content: articleContent,
+                        image: articleImg,
+                        date: articleDate
+                    })
+                }
+                let containerFavorite = document.querySelector('.container-favorite')
+                console.log(containerFavorite)
+                
+                // favori.classList.toggle("activated")
+
+                allFavorites.forEach(function(favoris) {
+                    let div = document.createElement('div')
+                    let article = `<div class="col-12 mt-5">
+                                        <div class="card article article${favoris.id}">
+                                            <div class="card-header ">
+                                                <h5 class="card-title d-flex justify-content-between">${favoris.title}<span class="publication-date">${favoris.publicationDate}</span></h5>
+                                            </div>
+                                            <img src="${favoris.image}" class="card-img-top">
+                                            <span class="fa-stack fa-2x addFavorite">
+                                                <i class="fas fa-star fa-stack-1x"></i>
+                                                <i class="far fa-star fa-stack-1x"></i>
+                                            </span>
+                                            <div class="card-body">
+                                                <p class="card-text">${favoris.content}</p>
+                                            </div>
+                                        </div>
+                                    </div>`
+                div.innerHTML = article;
+                containerFavorite.appendChild(div)
+                })
+
+            })
+
+        });
     })
     .catch(error => {
         console.error('Error:', error);
