c96676f1cb58ed25a9d2ba3b5c6908a101d47d31
Joris Bertier <joris.bertier@gmail.com>
2024-04-24 15:19:17 -0500

    favorite local storage

--- frontend/view/favorites/favorites.js
+++ frontend/view/favorites/favorites.js
@@ -1,3 +1,31 @@
 /**
  * Gère l'affichage et les interactions de la page des favoris
  */
+
+let container = document.querySelector('.container')
+console.log(container)
+
+let favorisJSON = localStorage.getItem('favoris');
+let storedFavoris = JSON.parse(favorisJSON);
+
+
+storedFavoris.forEach(favoris => {
+            let div = document.createElement('div')
+            let article = `<div class="col-12 mt-5">
+                                <div class="card article article${favoris.id}">
+                                    <div class="card-header ">
+                                        <h5 class="card-title d-flex justify-content-between">${favoris.title}<span class="publication-date">${favoris.publicationDate}</span></h5>
+                                    </div>
+                                    <img src="${favoris.image}" class="card-img-top">
+                                    <span class="fa-stack fa-2x addFavorite">
+                                        <i class="fas fa-star fa-stack-1x"></i>
+                                        <i class="far fa-star fa-stack-1x"></i>
+                                    </span>
+                                    <div class="card-body">
+                                        <p class="card-text">${favoris.content}</p>
+                                    </div>
+                                </div>
+                            </div>`
+        div.innerHTML = article;
+        container.appendChild(div)
+})
