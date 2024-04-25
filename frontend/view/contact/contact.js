5d178a877e0117ffd63b6931469301cdc4243bc2
Joris Bertier <joris.bertier@gmail.com>
2024-04-25 09:06:33 -0500

    add contition

--- frontend/view/contact/contact.js
+++ frontend/view/contact/contact.js
@@ -9,13 +9,34 @@ let form = document.querySelector('form')
 console.log(inputEmail)
 
 
+function validateName(name) {
+    if(name.length < 5 ||  name.length > 400) {
+        console.log('inferieure a 5 ou supereiure a 400')
+        return false;
+    }
+    return true
+}
+function validateEmail(email) {
+    let regexEmail = new RegExp('^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$');
+    if(!regexEmail.test(email)) {
+        console.log('email non valide')
+        return false
+    }
+    return true
+}
+
 form.addEventListener('submit', (event)=> {
     event.preventDefault()
-    let regexEmail = new RegExp('^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$');
-    if(!regexEmail.test(inputEmail)) {
-        console.log('email non valide')
+    
+    let isValidateEmail = validateEmail(inputEmail.value)
+    let isValidateName = validateName(inputName.value)
+
+    if(isValidateEmail && isValidateName) {
+        alert('Envoie de formulaire effectuer')
+        inputEmail.value =""
+        inputName.value =""
     }
-    if(inputName.value.length < 5 ||  inputName.value.length > 400) {
-        console.log('inferieure a 5 ou supereiure a 400')
-    }
+
+    
+
 })
