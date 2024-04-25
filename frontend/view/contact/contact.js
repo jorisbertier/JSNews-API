a8aa8db7aa9f2058a37657c50f1cab2f22e14001
Joris Bertier <joris.bertier@gmail.com>
2024-04-25 08:58:05 -0500

    condition email

--- frontend/view/contact/contact.js
+++ frontend/view/contact/contact.js
@@ -6,10 +6,15 @@
 let inputEmail = document.getElementById('email')
 let inputName = document.getElementById('name')
 let form = document.querySelector('form')
+console.log(inputEmail)
 
 
 form.addEventListener('submit', (event)=> {
     event.preventDefault()
+    let regexEmail = new RegExp('^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$');
+    if(!regexEmail.test(inputEmail)) {
+        console.log('email non valide')
+    }
     if(inputName.value.length < 5 ||  inputName.value.length > 400) {
         console.log('inferieure a 5 ou supereiure a 400')
     }
